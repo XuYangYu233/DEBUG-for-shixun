5481ec10a74b93aadbc1f2318030442ba29cc3af
XuYangYu233 <523191489@qq.com>
2020-06-26 00:16:53 +0800

    fix some bugs in 3/6-10

--- 3/10.c
+++ 3/10.c
@@ -1,66 +1,77 @@
 #include <stdio.h>
-
-int main() {
+// finish
+int main()
+{
     int matrix[100][100];
     int m;
     int n;
 
     int k = 0;
     int l = 0;
-    int num;
+    int i, j;
+
     scanf("%d %d", &m, &n);
-    
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            scanf("%d ", &matrix[i][j]);
+
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
         }
     }
 
-      
     while (k < m && l < n)
     {
         /* Print the first row from the remaining rows */
-        for (int i = l; i < n; i++)
+        for (i = l; i < n; i++)
         {
-            printf("%d ", matrix[k][i]);
-
+            printf("%d", matrix[k][i]);
+            if (i != n - 1 || k != m - 1)
+            {
+                printf(" ");
+            }
         }
         k++;
- 
-        /* Print the last column from the remaining columns */
-        for (int i = k; i < m; i++)
-        {
-            printf("%d ", matrix[i][n - 1]);
 
+        /* Print the last column from the remaining columns */
+        for (i = k; i < m; i++)
+        {
+            printf("%d", matrix[i][n - 1]);
+            if (i != m - 1 || l != n - 1)
+            {
+                printf(" ");
+            }
         }
         n--;
- 
+
         /* Print the last row from the remaining rows */
         if (k < m)
         {
-            for (int i = n - 1; i >= l; i--)
+            for (i = n - 1; i >= l; i--)
             {
-                printf("%d ", matrix[m - 1][i]);
- 
+                printf("%d", matrix[m - 1][i]);
+                if (i != l || k != m - 1)
+                {
+                    printf(" ");
+                }
             }
             m--;
         }
- 
+
         /* Print the first column from the remaining columns */
         if (l < n)
         {
-            for (int i = m - 1; i >= k; i--)
+            for (i = m - 1; i >= k; i--)
             {
                 printf("%d", matrix[i][l]);
-                if (i != k){
+                if (i != k || l != n - 1)
+                {
                     printf(" ");
+                }
             }
-
-            }
-            l++;    
-        }        
+            l++;
+        }
     }
-
 
     return 0;
 }
--- 3/7.c
+++ 3/7.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main() {
+// finish
+int main()
+{
     int matrix[100][100];
     int m;
     int n;
@@ -9,67 +11,82 @@ int main() {
     int startY;
     int endX;
     int endY;
-   
+
     startX = 0;
     startY = 0;
     endX = m;
     endY = n;
-   
+
     //输入行数m，列数n
     scanf("%d %d", &m, &n);
     // 输入一个m*n的矩阵
-    for (i = 0; i < m; i++){
-        for (j = 0; j < n; j++){
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
             scanf("%d", &matrix[i][j]);
         }
     }
-   
+
     // 判断循环圈数的条件
-    while (startX * 2 < m && startY * 2 < n) {
+    while (startX * 2 < m && startY * 2 < n)
+    {
         endX = m - 1 - startX;
         endY = n - 1 - startY;
         // 输出第一步
-        if (startY < endY) {
-            for (j = startY; j <= endY; j++) {
+        if (startY < endY)
+        {
+            for (j = startY; j <= endY; j++)
+            {
                 printf("%d", matrix[startX][j]);
-                if (j <= endY) {
-                    printf(" ");
+                if (j < endY || startX < endX)
+                {
+                    printf("*");
                 }
             }
+            startX++;
         }
-       
-          
+
         //满足以下条件输出第二步
-        if (startX < endX) {
-            for (j = startX + 1; j <= endX; j++) {
+        if (startX - 1 < endX)
+        {
+            for (j = startX; j <= endX; j++)
+            {
                 printf("%d", matrix[j][endY]);
-                if (j <= endY){
-                    printf(" ");
+                if (j < endX || endX > startX - 1 && startY < endY)
+                {
+                    printf("*");
                 }
             }
+            startX--;
         }
         //满足以下条件输出第三步
-        if (endX > startX && startY < endY) {
-            for (j = endY - 1; j >= startY; j--){
+        if (endX > startX && startY < endY)
+        {
+            for (j = endY - 1; j >= startY; j--)
+            {
                 printf("%d", matrix[endX][j]);
-                if (j >= startY){
-                    printf(" ");
+                if (j > startY || endX - 1 > startX && startY < endY)
+                {
+                    printf("*");
                 }
             }
         }
         //满足以下条件输出第四步
-        if (endX - 1 > startX && startY < endY) {
-            for (i = endX - 1; i >= startX + 1; i--){
-                printf("%d ", matrix[i][startX]);
-                if (i >= startX + 1){
-                    printf(" ");
+        if (endX - 1 > startX && startY < endY)
+        {
+            for (i = endX - 1; i >= startX + 1; i--)
+            {
+                printf("%d", matrix[i][startX]);
+                if (i > startX + 1 || startY + 1 < endY)
+                {
+                    printf("*");
                 }
             }
         }
         startX++;
         startY++;
     }
-    
-   
+
     return 0;
 }
