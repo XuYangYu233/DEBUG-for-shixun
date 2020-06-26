0347875e078450402040b9a23fee46eba8b0afad
XuYangYu233 <523191489@qq.com>
2020-06-27 00:33:24 +0800

    fix some bugs behind 13/6-10

--- 13/6.c
+++ 13/6.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node {
+// finish
+typedef struct node
+{
     int data;
     struct node *next;
 } Node;
@@ -8,7 +10,8 @@ typedef struct node {
 Node *circle_create(int n);
 void count_off(Node *head, int n, int k, int m);
 
-int main() {
+int main()
+{
     int n, k, m;
     scanf("%d%d%d", &n, &k, &m);
     Node *head = circle_create(n);
@@ -16,18 +19,20 @@ int main() {
     return 0;
 }
 
-Node *circle_create(int n) {
+Node *circle_create(int n)
+{
     Node *temp, *new_node, *head;
     int i;
 
     // 创建第一个链表节点并加数据
-    temp = (Node *) malloc(sizeof(Node));
+    temp = (Node *)malloc(sizeof(Node));
     head = temp;
     head->data = 1;
 
     // 创建第 2 到第 n 个链表节点并加数据
-    for(i = 2; i <= n; i++) {
-        new_node = (Node *) malloc(sizeof(Node));
+    for (i = 2; i <= n; i++)
+    {
+        new_node = (Node *)malloc(sizeof(Node));
         new_node->data = i;
         temp->next = new_node;
         temp = new_node;
@@ -39,31 +44,57 @@ Node *circle_create(int n) {
     return head;
 }
 
-void count_off(Node *head, int n, int k, int m) {
+void count_off(Node *head, int n, int k, int m)
+{
     int counter = 0;
     int i, j;
     Node *temp, *flag;
     temp = head;
-    while (counter < k - 1) {
+    while (counter < k - 1)
+    {
         temp = temp->next;
         counter++;
-    } 
-    //printf("temp->data = %d\n", temp->data);
-    if (m != 1) {
-    for (j = 0; j < n; j++) {
-        for (i = 0; i < m - 1; i++) {
-            if (i == m - 2) {
-                flag = temp;
-            }
-            temp = temp->next;
-        } 
-        printf("%d ", temp->data);
-        flag->next = temp->next;
-    	temp = temp->next;
     }
-    } else {
-        for (i = 1; i <= n; i++) {
-            printf("%d ", i);
+    //printf("temp->data = %d\n", temp->data);
+    if (m != 1)
+    {
+        for (j = 0; j < n; j++)
+        {
+            for (i = 0; i < m - 1; i++)
+            {
+                if (i == m - 2)
+                {
+                    flag = temp;
+                }
+                temp = temp->next;
+            }
+            printf("%d", temp->data);
+            if (j != n - 1)
+            {
+                printf(" ");
+            }
+            flag->next = temp->next;
+            free(temp);
+            temp = flag->next;
+        }
+    }
+    else
+    {
+        while (temp->data != k)
+        {
+            temp = temp->next;
+        }
+
+        for (i = 1; i <= n; i++)
+        {
+            printf("%d", temp->data);
+            if (i != n)
+            {
+                printf(" ");
+            }
+            flag = temp;
+            temp = temp->next;
+            free(flag);
         }
     }
     return;
--- 13/9.c
+++ 13/9.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node {
+// finish
+typedef struct node
+{
     int data;
     struct node *next;
 } Node;
@@ -8,7 +10,8 @@ typedef struct node {
 Node *circle_create(int n);
 void count_off(Node *head, int n, int k, int m);
 
-int main() {
+int main()
+{
     int n, k, m;
     scanf("%d%d%d", &n, &k, &m);
     Node *head = circle_create(n);
@@ -16,18 +19,20 @@ int main() {
     return 0;
 }
 
-Node *circle_create(int n) {
+Node *circle_create(int n)
+{
     Node *temp, *new_node, *head;
     int i;
 
     // 创建第一个链表节点并加数据
-    temp = (Node *) malloc(sizeof(Node));
+    temp = (Node *)malloc(sizeof(Node));
     head = temp;
     head->data = 1;
 
     // 创建第 2 到第 n 个链表节点并加数据
-    for(i = 2; i <= n; i++) {
-        new_node = (Node *) malloc(sizeof(Node));
+    for (i = 2; i <= n; i++)
+    {
+        new_node = (Node *)malloc(sizeof(Node));
         new_node->data = i;
         temp->next = new_node;
         temp = new_node;
@@ -39,41 +44,55 @@ Node *circle_create(int n) {
     return head;
 }
 
-void count_off(Node *head, int n, int k, int m) {
-   Node *temp, *pre;
+void count_off(Node *head, int n, int k, int m)
+{
+    Node *temp, *pre;
     int i;
     int times;
     times = 1;
     temp = head;
     pre = head;
-    
-    for(i = 1; i < k - 1 ; i++){
-        pre = pre->next;
+
+    if (k == 1)
+    {
+        for (i = 1; i < n; i++)
+        {
+            pre = pre->next;
+        }
     }
-    
-    for(i = 1; i < k; i++){
+    else
+    {
+        for (i = 1; i < k - 1; i++)
+        {
+            pre = pre->next;
+        }
+        temp = pre->next;
+    }
+
+    i = 1;
+
+    while (times <= n)
+    {
+        if (i == m)
+        {
+            printf("%d", temp->data);
+            pre->next = pre->next->next;
+            free(temp);
+            temp = pre;
+            i = 1;
+            if (times != n)
+            {
+                printf(" ");
+            }
+            times++;
+        }
+        else
+        {
+            i++;
+        }
         pre = temp;
         temp = temp->next;
     }
 
-    i = 1;
-    
-    while(times <= n){
-      if(i == m){
-        printf("%d", temp->data);
-        times++;
-        pre->next = pre->next->next;
-        temp = pre;
-        i = 1;
-         if(times != 1){
-             printf(" ");
-         }
-    }
-        pre = temp;
-        temp = temp->next;
-        i++;
-    }
-    
-    
     return;
 }
