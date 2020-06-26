51ffe7d1a707d45e2a7b6befae14c7a0f912c151
XuYangYu233 <523191489@qq.com>
2020-06-27 00:07:03 +0800

    fix some bugs to 9/6-10

--- 9/8.c
+++ 9/8.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
-
-int main() {
-    int n;
-    scanf("%d",&n);
-    int i=0;
-    int x;
-    int y;
-    int m=1;
-    int b=1;
-    for(x = 0; x <= n / 2; x++){
-        for(y = 0; y <= n / 3; y++){
-            if(x * 2 + y * 3 == n){
-                i++;
+// finish
+int main()
+{
+    long long int n;
+    scanf("%lld", &n);
+    long long int i = 0, j;
+    long long int x, y;
+    long long int a, b, m, k;
+    for (x = 0; x <= n / 2; x++)
+    {
+        for (y = 0; y <= n / 3; y++)
+        {
+            if (x * 2 + y * 3 == n)
+            {
+                if (x == 0 || y == 0)
+                    i++;
+                else
+                {
+                    a = 1;
+                    b = 1;
+                    m = x + y;
+                    k = x > y ? y : x;
+                    for (j = m; j >= m - k + 1; j--)
+                        a = a * j;
+                    for (j = 1; j <= k; j++)
+                        b = b * j;
+                    i += a / b;
+                }
             }
         }
     }
-    printf("%d",i);
+    printf("%lld", i);
     return 0;
 }
