9a8873cae03e36d85993adbc50badaf93d2ac8c7
XuYangYu233 <523191489@qq.com>
2020-06-27 00:16:36 +0800

    fix some bugs with 11/6-10

--- 11/6.c
+++ 11/6.c
@@ -1,39 +1,36 @@
-
 #include <stdio.h>
 #include <string.h>
-
-int main() {
-    char arr[32];
-    char str[32];
-    int bits[32];
+// finish
+int main()
+{
+    long long int arr[32];
+    long long int bits[32];
+    char str[500];
     char result[32];
-    
-    scanf("%s", &arr[32]);
-    char input;
-    int j = 0;
-    
-    for(int i = 0; i < 32; i++){
-        arr[i] = '0';
+
+    scanf("%s", &str);
+    int i, b;
+
+    for (i = 0; i < 32; i++)
+    {
+        arr[i] = 0;
     }
-    
-    while (scanf("%c", &input) != EOF) {
-        if (input == '\n'){
-            break;
-        }else if(j >= 32){
-            break;
-        }
-        arr[j % 32] = arr[j % 32] + (int)(input);
-        j++;
+
+    for (i = 1; i <= strlen(str); i++)
+    {
+        arr[i % 32] += str[i - 1];
     }
-   
-    for (int i = 0; i < 32; i++) {
+
+    for (i = 0; i < 32; i++)
+    {
         bits[i] = (arr[31 - i]) ^ (arr[i] << 1);
-        int b = (bits[i] % 85) + 34;
+        b = (bits[i] % 85) + 34;
         result[i] = (char)b;
     }
-    
-    for (int i = 0; i < 32; i++) {
-        printf("%c",result[i]);
+
+    for (i = 0; i < 32; i++)
+    {
+        printf("%c", result[i]);
     }
     return 0;
 }
