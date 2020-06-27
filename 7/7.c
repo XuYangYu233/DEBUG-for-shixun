0a1425482ba74d7c142c310a4656585007ebae49
XuYangYu233 <523191489@qq.com>
2020-06-27 16:16:39 +0800

    fix code style in 7

--- 7/7.c
+++ 7/7.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// finish
+
 int main()
 {
     int Year, Month, Date;
@@ -7,17 +7,15 @@ int main()
     scanf("%d-%d-%d", &Year, &Month, &Date);
 
     Leapyear = (((Year % 100 != 0 && Year % 4 == 0) || Year % 400 == 0) == 1 ? 1 : 0); //1代表闰年，2月有29日
-    Februaryvalid = (Leapyear == 0 && Month == 2 && Date < 29 && Date > 0) || (Leapyear == 1 && Month == 2 && Date < 30 && Date > 0);
+    Februaryvalid = (Leapyear == 0 && Month == 2 && Date < 29 && Date > 0)
+        || (Leapyear == 1 && Month == 2 && Date < 30 && Date > 0);
     Month30valid = (Month == (4 || 6 || 9 || 11)) && (Date < 31 && Date > 0);
     Month31valid = (Month == (1 || 3 || 5 || 7 || 8 || 10 || 12)) && (Date < 32 && Date > 0);
     Datevalid = Februaryvalid || Month30valid || Month31valid;
     //printf("%d\n%d\n%d\n",Month31valid,Month==1||3||5||7||8||10||12,Month==8);//调试用
-    if (Datevalid == 1)
-    {
+    if (Datevalid == 1) {
         printf("YES");
-    }
-    else
-    {
+    } else {
         printf("NO");
     }
     return 0;
