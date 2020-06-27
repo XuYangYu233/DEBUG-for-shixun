04776951b485d13f1e4e8633640b1a1a57ceedad
XuYangYu233 <57837734+XuYangYu233@users.noreply.github.com>
2020-06-27 14:36:29 +0800

    Update 10.c
    change style

--- 1/10.c
+++ 1/10.c
@@ -19,17 +19,16 @@ double bisection(int p, int q, double (*func)(int, int, double))
     double a = -20;
     double b = 20;
     double m;
-    while (1)
-    {
+    while (1) {
         m = (a + b) / 2;
-        if (fabs(func(p, q, m)) < EPSILON)
+        if (fabs(func(p, q, m)) < EPSILON) {
             return m;
-        else
-        {
-            if (func(p, q, m) * func(p, q, a) < 0)
+        } else {
+            if (func(p, q, m) * func(p, q, a) < 0) {
                 b = m;
-            else
+            } else {
                 a = m;
+            }
         }
     }
 }
